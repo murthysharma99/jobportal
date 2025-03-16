ba2f00522cd43a7e2857580d1abe3e706c7e7de2
sreed <sreed@DESKTOP-3DGV3SD.lan>
2025-03-16 17:52:59 +0530

    login security features

--- jobportal/src/main/java/com/luv2code/jobportal/entity/RecruiterProfile.java
+++ jobportal/src/main/java/com/luv2code/jobportal/entity/RecruiterProfile.java
@@ -42,6 +42,10 @@ public class RecruiterProfile {
         this.profilePhoto = profilePhoto;
     }
 
+    public RecruiterProfile(Users users) {
+        this.userId = users;
+    }
+
     public int getUserAccountId() {
         return userAccountId;
     }
